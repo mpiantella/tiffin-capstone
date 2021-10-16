4d09ad542d88bbf09f981edd12cef8ffe2d5a684
Maria Lucena <mpiantella@hotmail.com>
2021-10-15 23:16:52 -0400

    set endpoint

--- packages/ui/src/App.vue
+++ packages/ui/src/App.vue
@@ -1,70 +1,58 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-toolbar-title>Remote Brilliance</v-toolbar-title>
+      <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn text rounded>Login</v-btn>
+      **<v-btn
+        v-for="link in links"
+        :key="`${link.label}-header-link`"
+        text
+        rounded
+        :to="link.url"
+      >
+        {{ link.label }} </v-btn
+      >**
     </v-app-bar>
     <v-content>
-      <!-- Login Component-->
-      <v-card width="400" class="mx-auto mt-5">
-        <v-card-title>
-          <h1 class="display-1">Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field label="Username" prepend-icon="mdi-account-circle" />
-            <v-text-field
-              :type="showPassword ? 'text' : 'password'"
-              label="Password"
-              prepend-icon="mdi-lock"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            />
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <router-view></router-view>
     </v-content>
     <v-footer color="primary lighten-1" padless>
-      <v-row justify="center" no-gutters>
-        <v-btn
+      <v-layout justify-center wrap>
+        **<v-btn
           v-for="link in links"
-          :key="link"
+          :key="`${link.label}-footer-link`"
           color="white"
           text
           rounded
           class="my-2"
+          :to="link.url"
         >
-          {{ link }}
-        </v-btn>
-        <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
-          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-        </v-col>
-      </v-row>
+          {{ link.label }} </v-btn
+        >**
+        <v-flex primary lighten-2 py-4 text-center white--text xs12>
+          {{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
+        </v-flex>
+      </v-layout>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-// import Home from "./components/Home";
-
 export default {
   name: "App",
-
-  components: {
-    // Home,
+  data() {
+    return {
+      links: [
+        {
+          label: "Home",
+          url: "/",
+        },
+        {
+          label: "Login",
+          url: "/login",
+        },
+      ],
+    };
   },
-
-  data: () => ({
-    showPassword: false,
-    links: ["Home", "Login"],
-  }),
 };
 </script>
---
+++ packages/ui/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import Home from "@/components/Home.vue";
+
+export default {
+  name: "home",
+  components: {
+    Home,
+  },
+};
+</script>
