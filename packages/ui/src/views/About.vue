cb4e5f5cf9bea1d15645360011d4d8da793c93ac
Maria Lucena <mpiantella@hotmail.com>
2021-10-29 11:28:59 -0400

    About us

--- packages/ui/src/views/About.vue
+++ packages/ui/src/views/About.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <p>We are here for you</p>
+    <h1>Remote Brilliance</h1>
+    <p>
+      Needs a job that allows working remotely? Need to train for that job?
+      Remote brilliance is a portal for connecting with companies offering job
+      opportunities to work remotely. They or other companies may have programs
+      that allow to train for those roles or offer training free of charge. You
+      can always use the many career articles to help you build a plan to help
+      yourself get there. All you need is a device that connects to the
+      internet, and the persistence to do it.
+    </p>
+    <h1>Mission Statement</h1>
+    <p>
+      The main goal of this tool is to help women help themselves find a better
+      quality of life by finding online jobs, and if they need, free online
+      resources that could help them qualify for those jobs.
+    </p>
   </div>
 </template>
