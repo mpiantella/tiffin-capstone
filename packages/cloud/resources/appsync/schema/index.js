83234be85c03521e80c6a7d01a96a29b1775dfeb
Maria Lucena <mpiantella@hotmail.com>
2021-10-29 17:15:14 -0400

    include changes before amplify

---
+++ packages/cloud/resources/appsync/schema/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const fs = require('fs');
+// create schema from graphql files
+const rootSchema =
+    `
+schema {
+    query: Query
+    mutation: Mutation
+    subscription: Subscription
+}
+`;
+
+function loadFile() {
+    try {
+        const queriesSchema = fs.readFileSync(`${__dirname}/queries.graphql`, 'utf8');
+        const mutationsSchema = fs.readFileSync(`${__dirname}/mutations.graphql`, 'utf8');
+        const typesSchema = fs.readFileSync(`${__dirname}/types.graphql`, 'utf8');
+        const inputSchema = fs.readFileSync(`${__dirname}/mutations.graphql`, 'utf8');
+
+        const schema = `${rootSchema} ${queriesSchema} ${mutationsSchema} ${typesSchema} ${inputSchema}`
+
+        console.log(schema)
+        return schema;
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+function writeSchema(schema) {
+    try {
+        fs.writeFileSync(
+            `${process.env.PWD}/resources/appsync/schema.graphql`, schema
+        )
+        console.log('\n\t ********** Schema was succesfully created **********');
+        //file written successfully
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+const appendedSchemas = loadFile()
+writeSchema(appendedSchemas)
--- packages/ui/README.md
+++ packages/ui/README.md
@@ -17,18 +17,11 @@ yarn run serve
 ### Compiles and minifies for production
 
 ```
+# build for dist
 yarn run build
-```
-
-### Run your tests
-
-```
+# test 
 yarn run test
-```
-
-### Lints and fixes files
-
-```
+# lint
 yarn run lint
 ```
 
@@ -36,7 +29,23 @@ yarn run lint
 
 See [Configuration Reference](https://cli.vuejs.org/config/).
 
-# Auth 
+## Amplify Auth 
 
-This is using Cognito for authentication
-Pool-Id:  us-east-1_laoNihSYI
+TODO: this projetc was initialized with the `amplify` cli to leverage the `auth` module with `aws cognito`. See the [amplify](https://docs.amplify.aws/lib/auth/getting-started/q/platform/js/) documentation for details.
+
+## Sync S3 content
+
+Two buckets were set up. Only one has public assets, which is the one we sync each time the site is updated. The `npm run deploy` will trigger the build command, and ten the s3 sync command to push those assets up.
+
+```
+npm run deploy # in turn calls npm run build && npm run s3:deploy
+```
+
+```
+# Buckets associated to the remotebrilliance.link domain
+remotebrilliance.link
+www.remotebrilliance.link # public with assets
+```
+
+Public bucket s3 location: http://www.remotebrilliance.link.s3-website-us-east-1.amazonaws.com
+
